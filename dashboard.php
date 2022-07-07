3b604d99c70befc0f854fb04ad55d35b92fdc097
abdulwahid31 <wahid_@student.ub.ac.id>
2022-07-07 13:51:08 +0700

    Upload login and navbar

---
+++ dashboard.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="css/dashboardstyle.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+  </head>
+  <body>
+  
+    <div class="sidebar">
+      <ul class="nav">
+        <li id="dashboard">
+          <a href="#">
+            <i class="bi bi-house-door"></i>
+            <span>Dashboard</span>
+          </a>
+        </li>
+        <li>
+          <a href="login.php">
+            <i class="bi bi-briefcase"></i>
+            <span>Kegiatan</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="bi bi-image"></i>
+            <span>Dokumentasi</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="bi bi-journal-text"></i>
+            <span>Laporan</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="wrapper">
+      <div class="navbar">
+            <div class="left">
+              <img class="logo" src="images/iconnav.svg"/>
+            </div>
+            <div class="right">
+                <ul>
+                  <li>
+                    <a href="#">
+                      <p>wahid</p>
+                      <i class="bi bi-person-circle" id="usericon"></i>
+                      <i class="bi bi-caret-down-fill" id="dropicon"></i>
+                    </a>
+                    <div class="dropdown">
+                        <ul>
+                          <li id="profilebox">
+                            <a href="#">
+                              <i class="bi bi-person"></i>
+                              <span id="textprofile">Profile</span>
+                            </a>
+                          </li>
+                          <li>
+                            <a href="login.php">
+                              <i class="bi bi-box-arrow-right"></i>
+                              <span id="textsignout">Sign Out</span>
+                            </a>
+                          </li>
+                      </ul>
+                    </div> 
+                  </li>
+              </ul>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="main">
+    <h2>DASHBOARD</h2>
+    <P>Ini Halaman Dasgboard</P>
+    </div>
+    <script>
+    document.querySelector(".right ul li").addEventListener("click", function(){
+    this.classList.toggle("active");
+    });
+</script> 
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,71 @@
+<?php
+    session_start();
+    include "koneksi.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/loginstyle.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <title>Halaman Login</title>
+</head>
+<body>
+    <div class="container">
+        <h1>
+        <img src="images/imglogin.jpg" style="width: 120px;border-radius: 10px;"/>
+        </h1>
+            <form method="post">
+                <label>Username</label>
+                <input type="text" name="fusername" autofocus="" autocapitalize="none" autocomplete="username" required="" id="id_username"placeholder="Username">
+                </input>
+                <br>
+                <br>
+                <label>Password</label>
+                <input type="password" name="fpassword" autocomplete="current-password" required="" id="id_password"placeholder="Password">
+                    <span>
+                        <i id="togglePassword" class="far fa-eye"></i>
+                    </span>
+                </input>
+                <button type="submit" name="fmasuk">Masuk</button>
+            </form>
+        <h2>
+        Lupa Password?
+        <a href="https://google.com">klik disini</a>
+        </h2>
+        
+        <script>
+        const togglePassword = document.querySelector('#togglePassword');
+  const password = document.querySelector('#id_password');
+
+  togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
+        </script>
+
+        <?php
+        if (isset($_POST['fmasuk'])) {
+            $username = $_POST['fusername'];
+            $password = $_POST['fpassword'];
+            $qry = mysqli_query($koneksi,"SELECT * FROM tab_input WHERE username = '$username' AND password = md5('$password')");
+            $cek = mysqli_num_rows($qry);
+            if ($cek==1) {
+                $_SESSION['userweb']=$username;
+                header ("location: dashboard.php");
+                exit;
+            }
+            else{
+                echo "<center><font color= red>Maaf username atau password anda salah !</font></center>";
+            }
+        }
+        ?>
+    </div>   
+    </body>
+</html>
